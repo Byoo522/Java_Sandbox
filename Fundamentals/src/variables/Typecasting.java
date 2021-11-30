3c4d4cd93684c57f8a8f417472affd188dc4ba99
Benjamin Yoo <byoo0522@gmail.com>
2021-11-29 19:52:09 -0800

    finish typecasting

---
+++ Fundamentals/src/variables/Typecasting.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class Typecasting {
+	public static void main(String[] args) {
+		double foo = 2.95;
+		int bar = (int) foo;
+		
+		System.out.println(foo);
+		System.out.println(bar);
+		
+		int num1 = 17;
+		int num2 = 4;
+		
+		System.out.println((double) num1 / num2);
+	}
+}
