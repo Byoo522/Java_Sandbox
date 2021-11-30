e51ad35835d28d58170d5a1799a5754499750abc
Benjamin Yoo <byoo0522@gmail.com>
2021-11-29 19:47:10 -0800

    naming rules and best practices

--- Fundamentals/src/variables/CharBoolean.java
+++ Fundamentals/src/variables/CharBoolean.java
@@ -13,5 +13,18 @@ public class CharBoolean {
 		System.out.println(c);
 		System.out.println(d);
 		System.out.println(word + "as ice");
+		
+		// NAMING RULES AND BEST PRACTICES - literals are types by me.
+		int age = 27;
+		double statueHeightInMeters = 14.27;
+		boolean hasDriverLicense = true;
+		char userInput = 'z';
+		String name = "Benjamin";
+		
+		System.out.println(age);
+		System.out.println(statueHeightInMeters);
+		System.out.println(hasDriverLicense);
+		System.out.println(userInput);
+		System.out.println(name + "is dope");
 	}
 }
