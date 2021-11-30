be394c2397df80c261143d5f34dcd0cb522ee0b9
Benjamin Yoo <byoo0522@gmail.com>
2021-11-29 19:27:46 -0800

    int and doubles

---
+++ Fundamentals/src/variables/int_double.java
@@ -0,0 +1,10 @@
+package variables;
+
+public class int_double {
+	// Primitive Data Types
+	// byte, short, int, long, float, double, boolean, char
+	byte drum = 127;
+	int x = 2; // default for numbers
+	double y = 12.12; // default for decimals
+	
+}
--- Fundamentals/src/variables/intro.java
+++ Fundamentals/src/variables/intro.java
@@ -3,5 +3,11 @@ package variables;
 public class intro {
 	public static void main(String[] args) {
 		System.out.println("variables");
+		int x = 10;
+		System.out.println(x);
+		
+		// + - * / () - operators
+		int guitar = 10;
+		System.out.println(guitar + 5); // 15
 	}
 }
