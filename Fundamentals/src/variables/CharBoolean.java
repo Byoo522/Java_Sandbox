28a9b051be9263fc9f05dc9dfacdb0f4d2e01bd0
Benjamin Yoo <byoo0522@gmail.com>
2021-11-29 19:42:56 -0800

    finish charBoolean

---
+++ Fundamentals/src/variables/CharBoolean.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class CharBoolean {
+	public static void main(String[] args) {
+		int a = 27;
+		double b = 14.27;
+		boolean c = false;
+		char d = 'z';
+		String word = "Today is cold";
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(word + "as ice");
+	}
+}
