e8f4076dfc4e7c58c286e69875c67828cf472993
Benjamin Yoo <byoo0522@gmail.com>
2021-11-29 19:31:44 -0800

    complete int double

--- Fundamentals/src/variables/int_double.java
+++ Fundamentals/src/variables/int_double.java
@@ -1,10 +1,12 @@
 package variables;
 
 public class int_double {
-	// Primitive Data Types
-	// byte, short, int, long, float, double, boolean, char
-	byte drum = 127;
-	int x = 2; // default for numbers
-	double y = 12.12; // default for decimals
+	public static void main(String[] args) {
+		// Primitive Data Types
+		// byte, short, int, long, float, double, boolean, char
+		byte drum = 127;
+		int x = 2; // default for numbers
+		System.out.println(x + drum);
+	}
 	
 }
