defb3edbb8ff1e566c79e983f160d54c01a34eca
Benjamin Yoo <byoo0522@gmail.com>
2021-11-30 20:45:47 -0800

    switch case statement

---
+++ Fundamentals/src/ConditionalStatements/SwitchStatement.java
@@ -0,0 +1,23 @@
+package ConditionalStatements;
+
+import java.util.Scanner;
+
+public class SwitchStatement {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		
+		switch(num) {
+		case 1:
+			System.out.println("You entered one.");
+			break;
+		case 2:
+			System.out.println("You entered two.");
+			break;
+		default:
+			System.out.println("Invalid number");
+		}
+	}
+
+}
