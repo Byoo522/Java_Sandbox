d189573e14840d44bda22df3908625be9182ca2a
Benjamin Yoo <byoo0522@gmail.com>
2021-11-30 18:23:51 -0800

    user input scanner

---
+++ Fundamentals/src/variables/ScannerPractice.java
@@ -0,0 +1,17 @@
+package variables;
+
+import java.util.Scanner;
+
+public class ScannerPractice {
+	public static void main(String[] args) {
+		// Creates a scanner and stored in variable. System.in - telling system to read standar input(keyboard)
+		int age = 27;
+		double statueHeightInMeters = 14.72;
+		boolean hasInternetConnection = false;
+		char userInput = '\u005A';
+		String name = "Ben";
+		Scanner a = new Scanner(System.in);
+		String b = a.next(); // setting variable to the method to return data
+		System.out.println(b);
+	}
+}
