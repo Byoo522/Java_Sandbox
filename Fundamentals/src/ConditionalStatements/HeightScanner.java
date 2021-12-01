c0d7ae014f8e06e284285bfbbad328a8438f0443
Benjamin Yoo <byoo0522@gmail.com>
2021-11-30 20:22:34 -0800

    using or operand

--- Fundamentals/src/ConditionalStatements/HeightScanner.java
+++ Fundamentals/src/ConditionalStatements/HeightScanner.java
@@ -11,10 +11,16 @@ public class HeightScanner {
 		System.out.println("Please enter your height in centimeters: ");
 		int height = scanner.nextInt();
 		
-		if(height < 130) {
-			System.out.println("Sorry, you are too short.");
-		} else if (height > 210) {
-			System.out.println("Sorry, you are too tall.");
+//		if(height < 130) {
+//			System.out.println("Sorry, you do not meet the height requirement.");
+//		} else if (height > 210) {
+//			System.out.println("Sorry, you are too tall.");
+//		} else {
+//			System.out.println("Height Accepted");
+//		}
+		
+		if(height < 130 || height > 210) {
+			System.out.println("Sorry, you do not meet the height requirement.");
 		} else {
 			System.out.println("Height Accepted");
 		}
