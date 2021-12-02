3e6c5070e3d00c831cbfa40e39ae291027af1f95
Benjamin Yoo <byoo0522@gmail.com>
2021-12-01 20:16:14 -0800

    foreach loop demo

---
+++ Fundamentals/src/arrays/ForeachLoopDemo.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class ForeachLoopDemo {
+
+	public static void main(String[] args) {
+		double[] da = new double[] {25.92, 1.32, -76.24, 833.11, 76.884};
+		
+		for(double e: da) {
+			System.out.println(e);
+		}
+
+	}
+
+}
