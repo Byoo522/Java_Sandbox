6d6348d09b3d4b84933fbf766941f4dd69cc9892
Benjamin Yoo <byoo0522@gmail.com>
2021-12-01 16:56:08 -0800

    add while loop example

---
+++ Fundamentals/src/loops/LoopObject.java
@@ -0,0 +1,14 @@
+package loops;
+
+public class LoopObject {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while (i < 10) {
+			System.out.println("test");
+			i = i + 1;
+		}
+
+	}
+
+}
