aad3b658733aff1cde4d587904fd613abdf611c0
Benjamin Yoo <byoo0522@gmail.com>
2021-12-01 17:03:55 -0800

    for loop, do while, continue, break

--- Fundamentals/src/loops/LoopObject.java
+++ Fundamentals/src/loops/LoopObject.java
@@ -5,8 +5,16 @@ public class LoopObject {
 	public static void main(String[] args) {
 		int i = 0;
 		while (i < 10) {
-			System.out.println("test");
+			System.out.println("while loop test");
 			i = i + 1;
+		}
+		
+		for(int j = 0; j < 10; j++) {
+			if(j == 4) {
+				break; // stops
+				// continue - skips element
+			}
+			System.out.println("j");
 		}
 
 	}
