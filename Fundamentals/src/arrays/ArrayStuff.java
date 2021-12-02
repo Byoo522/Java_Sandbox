50c332d2ad6b7072c0b9d67157a387b712f199ea
Benjamin Yoo <byoo0522@gmail.com>
2021-12-01 19:52:29 -0800

    advanced for loops

--- Fundamentals/src/arrays/ArrayStuff.java
+++ Fundamentals/src/arrays/ArrayStuff.java
@@ -2,11 +2,20 @@ package arrays;
 
 public class ArrayStuff {
 	public static void main(String[] args) {
-		int[] b = {75, 29, 350, 7, 4192};
-		b[3] = 51; // this can reassign b at index 3 to new value of 51
-		System.out.println(b[2]);
-		for(int i = 0; i < 5; i++) {
-			System.out.println(b[i]);
+		int[] nums = {75, 29, 350, 7, 4192, 33};
+		// nums[3] = 51; // this can reassign b at index 3 to new value of 51
+		// System.out.println(nums[2]);
+		int count = 0;
+		int sumOfNums = 0;
+		
+		
+		for(int i = 0; i < nums.length; i++) {
+			if(nums[i] > 100) {
+				count++;
+				sumOfNums += nums[i];
+			}
 		}
+		int mean = sumOfNums / nums.length;
+		System.out.println("Total Count: " + count + ", SumOfNums: " + sumOfNums + ", Mean: " + mean);
  	}
 }
