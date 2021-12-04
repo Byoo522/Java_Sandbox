dd41fb7cfcb225d0286616460efe806b570c322f
Benjamin Yoo <byoo0522@gmail.com>
2021-12-04 11:16:28 -0800

    passing data to method

---
+++ Fundamentals/src/methods/MethodPractice1.java
@@ -0,0 +1,17 @@
+package methods;
+
+public class MethodPractice1 {
+	
+	public static void main(String[] args) {
+		int var;
+		var = 5;
+		foo(var, 57.5, "caramel");
+//		foo(1);
+//		foo(3);
+//		foo(555);
+	}
+	
+	public static void foo(int a, double b, String c) {
+		System.out.println(a + b + c); // 62.5caramel
+	}
+}
