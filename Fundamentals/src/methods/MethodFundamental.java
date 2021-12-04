5b030ff144f254d833e02459390a6be86df74267
Benjamin Yoo <byoo0522@gmail.com>
2021-12-04 11:10:46 -0800

    Method basic

---
+++ Fundamentals/src/methods/MethodFundamental.java
@@ -0,0 +1,24 @@
+package methods;
+
+public class MethodFundamental {
+
+	public static void main(String[] args) {
+		System.out.println(19);
+		bar();
+		foo();
+		System.out.println(24);
+		
+	}
+	
+	public static void foo() {
+		int x = 50;
+		System.out.println(x);
+		System.out.println(16);
+	}
+	
+	public static void bar() {
+		foo();
+		System.out.println(12);
+		
+	}
+}
